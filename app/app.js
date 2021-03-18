e7a84bda959c6b2f6a20067a182a9a5d713485f9
hjhwanggg <hhyejeong30@gmail.com>
2021-03-18 18:15:28 +0900

    프런트 단 javascript 만들기

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home);  
 
-module.exports = app;
+module.exports = app; 
